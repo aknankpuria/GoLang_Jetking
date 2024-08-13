c32e0f2609afaf89fa2fda533ca6794f00191445
Aslam Khan shah <aksi34012@gmail.com>
2024-08-13 11:55:01 +0530

    done

--- maps/main.go
+++ maps/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 
 	stdGrades := make(map[string]int) // initializing an empty map
@@ -13,7 +15,43 @@ func main() {
 
 	// printing the map
 	for key, value := range stdGrades {
-		println("initial Marks of", key, ":", value)
+		fmt.Println("initial Marks of", key, ":", value)
+	}
+	// accessing particular element
+	fmt.Println("Marks of urmal is:", stdGrades["urmal"])
+
+	// deleting an element
+	delete(stdGrades, "mangesh")
+	// printing the map
+	for key, value := range stdGrades {
+		fmt.Println("after deletion Marks of", key, ":", value)
+	}
+	// updating an element
+	stdGrades["urmal"] = 100
+	// printing the map
+	for key, value := range stdGrades {
+		fmt.Println("after updation Marks of", key, ":", value)
+	}
+
+	// cheking if the key exists
+	if grade, exists := stdGrades["urmal"]; exists {
+		fmt.Println("Marks of urmal is:", grade)
+	} else {
+		fmt.Println("Marks of urmal is not present")
+	}
+
+	if grade, exists := stdGrades["mangesh"]; exists {
+		fmt.Println("Marks of mangesh is:", grade)
+	} else {
+		fmt.Println("Marks of mangesh is not present")
+	}
+
+	// cheking the length of the map
+	fmt.Println("length of the map is:", len(stdGrades))
+
+	// iterating over the map
+	for key, value := range stdGrades {
+		fmt.Println("Marks of", key, ":", value)
 	}
 
 }
